3cda4e01731ba4338ffa04e166451259efbdcf17
Ali Toghiani <ali.toghiani.dev@gmail.com>
2023-11-12 20:54:18 +0330

    feat(questions/enums, models): implement question.nums & question.model

---
+++ src/app/features/questions/enums/question.enum.ts
@@ -0,0 +1,5 @@
+export enum AnswerStatusEnum {
+  CORRECT_SELECTED = 'correct_selected',
+  CORRECT_UNSELECTED = 'correct_unselected',
+  INCORRECT = 'incorrect'
+}
--- src/app/features/questions/models/question.model.ts
+++ src/app/features/questions/models/question.model.ts
@@ -1,12 +1,16 @@
+import {AnswerStatusEnum} from "../enums/question.enum";
+
 export interface QuestionModel {
   id: number
   title: string
   answers: AnswerModel[]
   correct_answer_ids: number[]
+  point: number
 }
 
 export interface AnswerModel {
   id: number,
   label: string,
-  value: string
+  value: string,
+  status?: AnswerStatusEnum
 }
